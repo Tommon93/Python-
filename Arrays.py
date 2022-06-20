83e087e7cb7e86552b7981971d0f269514573b17
Thomas Le <thomasle093@gmail.com>
2022-06-20 18:51:04 +1000

    Update learning data structures

--- Arrays.py
+++ Arrays.py
@@ -14,3 +14,23 @@ for n in (new_list):
         print(True)
 
         break
+
+#Arrays - empty lists 
+numbers = [] #Creates an empty list of space of size n+1
+#The space allocated by the list and the space used by the list is not the same. 
+len(numbers) #Does not use memory allocation as an indicator of it's size - because it has space for 1 element
+numbers.append(2)
+numbers.append(200) #Python is resizing it's allocated memory to accomdate the extra element - called: list resizing 
+#Creates four contiguous blocks of memory for the list resizing (see below)
+#The growth pattern for lists in python is: 0,4,8,16,25,35,46... 
+#So when the elements are approaching these values - the list resizes again
+
+#Extend
+numbers = []
+numbers.extend([4,5,6])
+numbers
+#Extend takes another list to add - essentially takes a series of appends calls to each of the elements in the new list untili all of them are
+#appended to the original list. 
+#This operation has a run time of O(k)
+
+
